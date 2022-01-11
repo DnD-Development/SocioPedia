1cbe81a16a6a7d4b6c3817bb12b9ef6bc3cd9505
Devarshi Patel <yehh.amazing@gmail.com>
2022-01-11 14:15:51 +0530

    You can now Like-Dislike and get posts

--- routes/posts.js
+++ routes/posts.js
@@ -34,7 +34,7 @@ router.delete("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
       await post.deleteOne();
-      res.status(200).json("The Post has been Deleted");
+      res.status(200).json("The Post has been Deleted !!");
     } else {
       res.status(403).json("You can Delete only your Post !!");
     }
@@ -43,7 +43,33 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-// Like a Post
+// Like-Dislike a Post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked !!");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked !!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get Post
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Get Timeline
 
