fedf00bf8330023eabac333af69cb8b5ca8e6d41
Dhruv Garg <dhruvgargg02@gmail.com>
2022-01-12 20:29:01 +0530

    Topbar Done !!

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react";
+import Topbar from "../../components/topbar/Topbar";
 
 function Home() {
-  return (
-    <div>
-      <div>Hello THis is Home Page</div>
-    </div>
-  );
+  return <Topbar />;
 }
 
 export default Home;
