4dd6d0ec753321426dc0eabf1b14f16fdf6caffc
Dhruv Garg <dhruvgargg02@gmail.com>
2022-01-20 12:36:04 +0530

    Sorted Feed

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -30,12 +30,20 @@ function Profile() {
           <div className="profileRightTop">
             <div className="profileCover">
               <img
-                src={user.coverPicture || PF + `person/noCover.png`}
+                src={
+                  user.coverPicture
+                    ? PF + user.coverPicture
+                    : PF + `person/noCover.png`
+                }
                 alt={PF + `person/noCover.png`}
                 className="profileCoverImg"
               />
               <img
-                src={user.profilePicture || PF + `person/noAvatar.png`}
+                src={
+                  user.profliePicture
+                    ? PF + user.profliePicture
+                    : PF + `person/noAvatar.png`
+                }
                 alt=""
                 className="profileUserImg"
               />
